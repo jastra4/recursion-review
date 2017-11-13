6b2c6ea88cc5c7d7f5d77046482d5beb8b0f9124
jastra4 <josephstrandmo@gmail.com>
2017-11-13 16:28:37 -0600

    attempted stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,16 +7,31 @@ var stringifyJSON = function(obj) {
   // return a JSON string of the parameter
   //var result = "";
   // check data type of obj
-  if (typeof obj === 'number')
-    return "\"" + obj + "\"";
-     else if (typeof obj === 'string')
+  if (obj === null){
+    return "null";
+  }
+  if (Array.isArray(obj)) {
+    var result = "";
+    for (var i = 0; i < obj.length; i++) {
+      result.concat(",");
+    }
+    result.slice(0,result.length-1);
+    return "[" + result + "]";
+  } else if (typeof obj === 'object') {
+    var result = "";
+    for (var key in obj) {
+      // add key in brackets to result
+      // add : and space to result
+      // add property to result
+      result.concat(stringifyJSON(key) + ":" + stringifyJSON(obj[key]) + ",");
+    }
+    result.slice(0,result.length-1);
+    return "{" + result + "}";
+    
+  } else if (typeof obj === 'string')
       return "\"" + obj + "\"";
-       else if (typeof obj === 'boolean')
-         return "\"" + number + "\"";
-        else if (obj === null)
-          return "\"" + "null" + "\"";
-            
-        
-  // make string 
-//return result;
+        else if (typeof obj === 'undefined')
+           return "undefined";
+            else 
+              return obj.toString();
 };
