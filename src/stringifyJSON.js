6516934397f1dc7fc8d47a3f98b7fd182b95bf91
jastra4 <josephstrandmo@gmail.com>
2017-11-13 18:21:11 -0600

    Solved stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,31 +7,31 @@ var stringifyJSON = function(obj) {
   // return a JSON string of the parameter
   //var result = "";
   // check data type of obj
-  if (obj === null){
+if (obj === null){
     return "null";
-  }
-  if (Array.isArray(obj)) {
+  } else if (Array.isArray(obj)) {
+  
     var result = "";
     for (var i = 0; i < obj.length; i++) {
-      result.concat(",");
+      result += stringifyJSON(obj[i]);
+      result += ",";
     }
-    result.slice(0,result.length-1);
+    result = result.slice(0,result.length-1);
     return "[" + result + "]";
+   
   } else if (typeof obj === 'object') {
-    var result = "";
-    for (var key in obj) {
-      // add key in brackets to result
-      // add : and space to result
-      // add property to result
-      result.concat(stringifyJSON(key) + ":" + stringifyJSON(obj[key]) + ",");
-    }
-    result.slice(0,result.length-1);
+      var result = "";
+      for (var key in obj) {
+        if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+          result+= (stringifyJSON(key) + ":" + stringifyJSON(obj[key]) + ",");
+        }
+      }
+    result = result.slice(0,result.length-1);
     return "{" + result + "}";
-    
-  } else if (typeof obj === 'string')
-      return "\"" + obj + "\"";
-        else if (typeof obj === 'undefined')
-           return "undefined";
-            else 
-              return obj.toString();
+    } else if (typeof obj === 'string'){
+          return "\"" + obj + "\"";
+        } else if (obj === undefined) {
+               return 'test';
+             }
+             return obj.toString();
 };
